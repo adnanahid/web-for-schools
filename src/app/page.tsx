bb9638c4b7db3dbb585f3c580202ce557d0ddd24
Adnan <sikuahamed@gmail.com>
2025-08-02 12:28:44 +0600

    remove unnecessary import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import CommitteeMembers from "@/components/HomePage/CommitteeMembers";
 import CurriculumInfo from "@/components/HomePage/CurriculumInfo";
-import Footer from "@/components/HomePage/Footer";
 import HeadmasterWelcome from "@/components/HomePage/HeadmasterWelcome";
 import HeroSlider from "@/components/HomePage/HeroSlider";
-import Navigation from "@/components/HomePage/Navigation";
 import NoticeBoard from "@/components/HomePage/NoticeBoard";
 import RecentResults from "@/components/HomePage/RecentResults";
 
